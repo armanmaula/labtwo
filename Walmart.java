34e8bde820b6ef69c8868a7e57d740067fcd9a6d
armanmaula <57684879+armanmaula@users.noreply.github.com>
2020-02-27 17:03:08 -0500

    Create Walmart.java

---
+++ Walmart.java
@@ -0,0 +1,18 @@
+package labTwo;
+
+public class Walmart {
+
+	public static void main(String[] args) {
+
+		InvOrg bananas = new InvOrg ("Bananas", 3.00, 130);
+		Department chicken = new Department ("Chicken", 5.00, 190, "Produce");
+
+		
+
+
+		chicken.sentence();
+
+
+	}
+
+}
